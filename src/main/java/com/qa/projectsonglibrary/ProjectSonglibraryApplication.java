cb7dd75016f6e812da5c71deb1589dbdd82d9645
lanyamd2 <laniangel12@gmail.com>
2022-09-15 16:37:02 +0100

    Initial commit

---
+++ src/main/java/com/qa/projectsonglibrary/ProjectSonglibraryApplication.java
@@ -0,0 +1,17 @@
+package com.qa.projectsonglibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSonglibraryApplication {
+	//PutMapping???
+	//Junit testing
+	//html -> not completed but so far working
+	//lombok annotations
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSonglibraryApplication.class, args);
+	}
+
+}
